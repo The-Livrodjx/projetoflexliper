d5336a5a59ba0c6933427d9f76df8ac2b47a7bf5
The-Livrodjx <45701263+The-Livrodjx@users.noreply.github.com>
2020-01-31 23:43:46 -0300

    Add files via upload

---
+++ simpleFlex/backup.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Flexliper - Inteligência em TI</title>
+
+    <!--Site feito por Livrodjx, usando PHP e Bootstrap :) -->
+
+    <!-- Bootstrap -->
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--<link rel="stylesheet" href="css/style.css">-->
+
+    <link rel="stylesheet" href="css/backup.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="icon" type="imagem/png" href="images/fotoefeitos.com_.jpg" />
+ 
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top" style="background-color: white;">
+        <div class="container" style="background-color: white;">
+          <div class="navbar-header" style="background-color: white;">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="http://www.flexliper.com.br" style="background-color: white">Flexliper</a>
+          </div>
+          <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right" style="background-color: white;">
+              <li><a href="http://www.flexliper.com.br">Home</a></li>
+              <li><a href="/sobre">Sobre</a></li>
+              <li role="presentation" class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                Serviços <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="/marketing">Marketing</a></li>
+                <li><a href="/backup">Backup</a></li>
+                <li><a href="/suporte">Suporte Técnico</a></li>
+                <li><a href="/cftv">CFTV</a></li>
+              </ul>
+            </li>
+              <!--<li><a href="/marketing">Marketing</a></li>-->
+              <li><a href="/contato">Contato</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <div class="box">
+      <section class="banner" id="home">
+        <div class="overlay"></div>
+        <div class="container chamada-banner">
+          <div class="row">
+
+          </div><!--row-->
+        </div>
+      </section>
+    </div>
+
+    <section class="Zed">
+
+      <div class="row text-center" style="margin-bottom: 50px;">
+        <div class="col-md-12">
+          <blockquote>
+            <h2>O que é Backup ?</h2>
+            <p>Como definição, backup é o ato de copiar arquivos, pastas ou discos inteiros (físicos ou virtuais) para sistemas de armazenamento secundários, buscando a preservação dos dados em caso de qualquer problema.</p>
+          </blockquote>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-md-12">
+          <blockquote>
+            <h2>Por que a sua empresa precisa fazer backups constantes de servidores ?</h2>
+            <p> 1. Possibilita a restauração imediata dos dados<br>
+                2. Elimina o trabalho de recriar todos as informações<br>
+                3. Demonstra a conformidade da empresa com os regulamentos<br>
+                4. Gera vantagens competitivas para o negócio<br><br>
+
+                Backups periódicos evitam a perda de dados.
+                Outra ação essencial, que pode salvar o seu negócio frente a uma eventual perda de dados, é o backup. Seja em razão de um ataque de hackers ou até mesmo por uma pane no próprio computador, o backup de dados garante que seus arquivos estejam sempre a salvo.
+                Portanto, em parceria com o suporte técnico em TI, estabeleça uma rotina de backup para os computadores do seu negócio. O mais indicado, de acordo com as melhores práticas do setor, é realizar esse processo diariamente.
+                Caso seja inviável executar backups nessa frequência, a dica é avaliar qual volume de entrada de novas informações no negócio. Assim será possível definir a rotina ideal para essa ação.
+                Lembre-se apenas de não deixar que se acumulem muitos arquivos para o backup. Pois, se durante esse meio tempo alguma pane ocorrer, o prejuízo com a perda de informações certamente será maior.
+            </p>
+          </blockquote>
+        </div>
+      </div>
+    </section>
+
+    <hr class="featurette-divider">
+
+      <div class="row featurette" style="margin-bottom: 10px;">
+        <div class="col-md-7 col-md-push-5 text-center">
+          <h2 class="featurette-heading flexh">Quer proteger os seus dados ?</h2>
+          <p class="lead flex">A Flexliper disponibiliza um serviço completo de Backup, assegurando a proteção dos seus dados, venha dar esse passo tecnológico conosco.</p>
+          <a class="btn btn-primary btn-lg" href="/contato" role="button">Quero conhecer</a>
+        </div>
+        <div class="col-md-5 col-md-pull-7 yasuo">
+          <img class="featurette-image img-responsive center-block" style="height: 310px;width:100%;" src="images/backup2.jpg" alt="Aumente o valor da sua empresa contratando nossos serviços">
+        </div>
+      </div>
+
+      
+
+    <footer>
+      <p>Todos os direitos reservados - contato@flexliper.com.br</p>
+    </footer>
+      
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+      <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ simpleFlex/suporte.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Flexliper - Inteligência em TI</title>
+
+    <!--Site feito por Livrodjx, usando PHP e Bootstrap :) -->
+
+    <!-- Bootstrap -->
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--<link rel="stylesheet" href="css/style.css">-->
+    <link rel="stylesheet" href="css/suporte.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="icon" type="imagem/png" href="images/fotoefeitos.com_.jpg" />
+ 
+  </head>
+  <body>
+    <nav class="navbar navbar-default navbar-fixed-top" style="background-color: white;">
+        <div class="container" style="background-color: white;">
+          <div class="navbar-header" style="background-color: white;">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="http://www.flexliper.com.br" style="background-color: white">Flexliper</a>
+          </div>
+          <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right" style="background-color: white;">
+              <li><a href="http://www.flexliper.com.br">Home</a></li>
+              <li><a href="/sobre">Sobre</a></li>
+              <li role="presentation" class="dropdown">
+              <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                Serviços <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="/marketing">Marketing</a></li>
+                <li><a href="/backup">Backup</a></li>
+                <li><a href="/suporte">Suporte Técnico</a></li>
+                <li><a href="/cftv">CFTV</a></li>
+              </ul>
+            </li>
+              <!--<li><a href="/marketing">Marketing</a></li>-->
+              <li><a href="/contato">Contato</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+    </nav>
+
+    <div class="box">
+      <section class="banner" id="home">
+        <div class="overlay"></div>
+        <div class="container chamada-banner">
+          <div class="row">
+
+          </div><!--row-->
+        </div>
+      </section>
+    </div>
+
+    <section class="veigar">
+
+      <div class="row text-center">
+        <div class="col-md-12">
+          <blockquote>
+            <h2>O que é suporte técnico ?</h2>
+            <p>O suporte em TI é uma gama de serviços prestados com o objetivo de fornecer assistência a uma infraestrutura tecnológica. Tem a responsabilidade de garantir o funcionamento de ferramentas, servidores, máquinas/equipamentos e sistemas de TI de uma empresa.
+             Ele também deve fornecer auxílio para a implementação de processos seguros que garantam a proteção de dados, construindo métodos para melhores práticas de uso dessas ferramentas e soluções.</p>
+          </blockquote>
+        </div>
+
+      <div class="row text-center">
+        <div class="col-md-12">
+          <blockquote>
+            <h2>Como o suporte técnico em TI ajuda o seu Negócio a não parar?</h2>
+            
+            <p>Qual é a importância que o suporte técnico de Tecnologia da Informação (TI) tem para o seu negócio? Se você costuma acioná-lo apenas em situações emergenciais, é hora de adotar novos hábitos. Quando se trata do setor de TI, independente do segmento de negócio, é preciso ter em mente que as ações preventivas são a melhor maneira de se evitar problemas.</p>
+            <p>Nesse cenário, o suporte técnico em TI pode ajudar o seu negócio fortalecer sua rede contra invasões semelhantes.</p>
+            <p>No combate à ameaça digitais, como vírus e ataques de digitais, o primeiro passo é certificar-se que a sua rede de computadores está realmente protegida. Para tanto, é imprescindível o uso do combo de.</p>
+            <p><a>Firewall</a></p>
+            <p><a>Antivírus</a></p>
+            <p>O suporte técnico em TI será valioso na escolha da opção de antivírus adequada para o negócio. Apesar de existirem inúmeras alternativas gratuitas no mercado, o mais recomendado é fazer uso de uma versão completa. O que implica o custo de assinatura, porém agrega recursos extras para a proteção da rede.</p>
+            <p>O trabalho de escolher o antivírus ideal para as necessidades do negócio será do profissional de TI. Ele também poderá explorar todas as soluções da ferramenta, garantindo total proveito do investimento.</p>
+            <p>É importante definir um calendário para varredura profunda dos computadores, com o apoio de um suporte especializado. Com uma investigação minuciosa é mais provável identificar problemas que possam parar suas atividades.</p>
+
+            <p>A importância da manutenção preventiva</p>
+            <p>É compreensível que, em seu dia a dia, a preocupação com a área de TI não seja uma urgência. Afinal, o que deve ser priorizado são as atividades ligadas diretamente a sua especialidade. Mas, para evitar que o seu computador pare inesperadamente e prejudique toda a sua rotina, programe um check-up geral em sua máquina.</p>
+            <p>A manutenção preventiva deve ser feita periodicamente em equipamentos que já estão fora da garantia.</p>
+            <p>Entre as ações que o suporte técnico em TI pode executar, estão a varredura com o antivírus e a atualização de drivers. Caso o computador esteja lento, a restauração do equipamento é recomendada, para recuperar a sua velocidade. Procure realizar esse processo anualmente.
+              Todas essas práticas são executadas sem tomar muito tempo do usuário. A manutenção preventiva, inclusive, pode ser feita de forma remota, ou seja, a longa distância.</p>
+          </blockquote>
+        </div>
+      </div>
+
+      </div>
+    </section>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette" style="margin-bottom: 10px;">
+      <div class="col-md-7 col-md-push-5 text-center">
+        <h2 class="featurette-heading flexh">Quer que sua empresa continue a progredir ?</h2>
+        <p class="lead flex">A Flexliper disponibiliza um serviço completo de Suporte técnico, assegurando a melhoria de processos e proteção contra invasões, venha dar esse passo tecnológico conosco.</p>
+        <a class="btn btn-primary btn-lg" href="/contato" role="button">Quero conhecer</a>
+      </div>
+      <div class="col-md-5 col-md-pull-7 yasuo">
+        <img class="featurette-image img-responsive center-block" style="height: 310px;width:100%;" src="images/suporte.jpeg" alt="Aumente o valor da sua empresa contratando nossos serviços">
+      </div>
+    </div>
+
+    <footer>
+        <p>Todos os direitos reservados - contato@flexliper.com.br</p>
+    </footer>
+     
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
